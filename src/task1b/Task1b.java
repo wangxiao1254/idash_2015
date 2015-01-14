6ab0b929b5f1c3ee4821ea1077ffdedb1f7b0f09
Xiao Wang <wangxiao1254@gmail.com>
2015-01-14 15:41:18 +0800

    .

--- src/task1b/Task1b.java
+++ src/task1b/Task1b.java
@@ -1,17 +1,16 @@
 package task1b;
 
-import java.util.Arrays;
-
 import util.EvaRunnable;
 import util.GenRunnable;
 import util.Utils;
 import circuits.arithmetic.FixedPointLib;
 import circuits.arithmetic.IntegerLib;
 import flexsc.CompEnv;
+import flexsc.Flag;
 
 public class Task1b {
 	static public int Width = 9;
-	static public int FWidth = 40;
+	static public int FWidth = 35;
 	static public int FOffset = 20;
 
 	public static class Generator<T> extends GenRunnable<T> {
@@ -38,9 +37,6 @@ public class Task1b {
 				controlData[i][1] = Utils.fromInt(controlSta[i].numOfG1G2, Width);
 				controlData[i][2] = Utils.fromInt(controlSta[i].numOfG2G2, Width);
 			}
-
-			System.out.println("Case"+caseSta[0].numOfG1G1+"\t"+caseSta[0].numOfG1G2+"\t"+caseSta[0].numOfG2G2);
-			System.out.println("Control"+controlSta[0].numOfG1G1+"\t"+controlSta[0].numOfG1G2+"\t"+controlSta[0].numOfG2G2);
 			aliceCase = gen.inputOfAlice(caseData);
 			aliceControl = gen.inputOfAlice(controlData);
 			bobCase = gen.inputOfBob(caseData);
@@ -50,6 +46,91 @@ public class Task1b {
 
 		T[][] res;
 		FixedPointLib<T> flib;
+		@Override
+		public void secureCompute(CompEnv<T> gen) {
+			res = gen.newTArray(numOfTests, 0);
+			IntegerLib<T> lib = new IntegerLib<T>(gen);
+			flib = new FixedPointLib<T>(gen, FWidth, FOffset);
+			// TODO Auto-generated method stub
+			for(int i = 0; i < numOfTests; ++i) {
+				T[][] caseFre = gen.newTArray(3, 0);
+				T[][] controlFre = gen.newTArray(3, 0);
+				for(int j = 0; j < 3; ++j) {
+					caseFre[j] = lib.add(aliceCase[i][j], bobCase[i][j]);
+					controlFre[j] = lib.add(aliceControl[i][j], bobControl[i][j]);
+				}
+
+				T[] resCase = flib.publicValue(0);
+				T[] resControl = flib.publicValue(0);
+				for(int k = 0; k < 3; ++k) {
+					T[] sq = lib.multiplyFull(caseFre[k], caseFre[k]);
+					T[] sq2 = lib.multiplyFull(controlFre[k], controlFre[k]);
+					
+					T[] sum = lib.add(caseFre[k], controlFre[k]);
+					//compute sum_i sq_i/sum_i
+					T[] fsq = lib.padSignal(sq, FWidth);
+					fsq = lib.leftPublicShift(fsq, FOffset);
+					T[] fsq2 = lib.padSignal(sq2, FWidth);
+					fsq2 = lib.leftPublicShift(fsq2, FOffset);
+					T[] fsum = lib.padSignal(sum, FWidth);
+					fsum= lib.leftPublicShift(fsum, FOffset);					
+					T[] div = flib.div(fsq, fsum);
+					resCase = flib.add(resCase, div);
+
+					T[] div2 = flib.div(fsq2, fsum);
+					resControl = flib.add(resControl, div2);
+				}
+				resCase = flib.multiply(resCase, flib.publicValue(1.0/200));
+				resControl = flib.multiply(resControl, flib.publicValue(1.0/200));
+				res[i] = flib.add(resCase, resControl);
+				System.out.println(i+" "+Flag.sw.ands);
+			}
+		}
+
+		@Override
+		public void prepareOutput(CompEnv<T> gen) {
+			System.out.println(numOfTests);
+			for(int i = 0; i < numOfTests; ++i)
+				System.out.println(flib.outputToAlice(res[i]));
+		}
+
+	}
+
+	public static class Evaluator<T> extends EvaRunnable<T> {
+
+		T[][][] aliceCase;
+		T[][][] bobCase;
+		T[][][] aliceControl;
+		T[][][] bobControl;
+		FixedPointLib<T> flib;
+		int numOfTests;
+		@Override
+		public void prepareInput(CompEnv<T> gen) {
+			Statistics[] caseSta = PrepareData.readFile(args[0], 1);
+			Statistics[] controlSta = PrepareData.readFile(args[1], 1);
+			boolean[][][] caseData = new boolean[caseSta.length][3][Width];
+			for(int i = 0; i < caseSta.length; ++i) {
+				caseData[i][0] = Utils.fromInt(caseSta[i].numOfG1G1, Width);
+				caseData[i][1] = Utils.fromInt(caseSta[i].numOfG1G2, Width);
+				caseData[i][2] = Utils.fromInt(caseSta[i].numOfG2G2, Width);
+			}
+
+			boolean[][][] controlData = new boolean[controlSta.length][3][Width];
+			for(int i = 0; i < caseSta.length; ++i) {
+				controlData[i][0] = Utils.fromInt(controlSta[i].numOfG1G1, Width);
+				controlData[i][1] = Utils.fromInt(controlSta[i].numOfG1G2, Width);
+				controlData[i][2] = Utils.fromInt(controlSta[i].numOfG2G2, Width);
+			}
+
+
+			aliceCase = gen.inputOfAlice(caseData);
+			aliceControl = gen.inputOfAlice(controlData);
+			bobCase = gen.inputOfBob(caseData);
+			bobControl = gen.inputOfBob(controlData);
+			numOfTests = caseSta.length;
+		}
+		T[][] res;
+
 		@Override
 		public void secureCompute(CompEnv<T> gen) {
 			res = gen.newTArray(numOfTests, 0);
@@ -96,91 +177,7 @@ public class Task1b {
 		@Override
 		public void prepareOutput(CompEnv<T> gen) {
 			for(int i = 0; i < numOfTests; ++i)
-				System.out.println(flib.outputToAlice(res[i]));
-		}
-
-	}
-
-	public static class Evaluator<T> extends EvaRunnable<T> {
-
-		T[][][] aliceCase;
-		T[][][] bobCase;
-		T[][][] aliceControl;
-		T[][][] bobControl;
-		FixedPointLib<T> flib;
-		int numOfTests;
-		@Override
-		public void prepareInput(CompEnv<T> gen) {
-			Statistics[] caseSta = PrepareData.readFile(args[0], 1);
-			Statistics[] controlSta = PrepareData.readFile(args[1], 1);
-			boolean[][][] caseData = new boolean[caseSta.length][3][Width];
-			for(int i = 0; i < caseSta.length; ++i) {
-				caseData[i][0] = Utils.fromInt(caseSta[i].numOfG1G1, Width);
-				caseData[i][1] = Utils.fromInt(caseSta[i].numOfG1G2, Width);
-				caseData[i][2] = Utils.fromInt(caseSta[i].numOfG2G2, Width);
-			}
-
-			boolean[][][] controlData = new boolean[controlSta.length][3][Width];
-			for(int i = 0; i < caseSta.length; ++i) {
-				controlData[i][0] = Utils.fromInt(controlSta[i].numOfG1G1, Width);
-				controlData[i][1] = Utils.fromInt(controlSta[i].numOfG1G2, Width);
-				controlData[i][2] = Utils.fromInt(controlSta[i].numOfG2G2, Width);
-			}
-
-
-			aliceCase = gen.inputOfAlice(caseData);
-			aliceControl = gen.inputOfAlice(controlData);
-			bobCase = gen.inputOfBob(caseData);
-			bobControl = gen.inputOfBob(controlData);
-			numOfTests = caseSta.length;
-		}
-		T[][] res;
-
-		@Override
-		public void secureCompute(CompEnv<T> gen) {
-			res = gen.newTArray(numOfTests, 0);
-			IntegerLib<T> lib = new IntegerLib<T>(gen);
-			flib = new FixedPointLib<T>(gen, FWidth, FOffset);//10e-6
-			// TODO Auto-generated method stub
-			for(int i = 0; i < numOfTests; ++i) {
-				T[][] caseFre = gen.newTArray(3, 0);
-				T[][] controlFre = gen.newTArray(3, 0);
-				for(int j = 0; j < 3; ++j) {
-					caseFre[j] = lib.add(aliceCase[i][j], bobCase[i][j]);
-					controlFre[j] = lib.add(aliceControl[i][j], bobControl[i][j]);
-				}
-
-				T[] resCase = flib.publicValue(0);
-				T[] resControl = flib.publicValue(0);
-				for(int k = 0; k < 3; ++k) {
-					T[] sq = lib.multiplyFull(caseFre[0], caseFre[0]);
-					T[] sq2 = lib.multiplyFull(controlFre[0], controlFre[0]);
-
-					T[] sum = lib.add(caseFre[0], controlFre[0]);
-					//compute sum_i sq_i/sum_i
-					T[] fsq = lib.padSignal(sq, FWidth);
-					fsq = lib.leftPublicShift(fsq, FOffset);
-					T[] fsq2 = lib.padSignal(sq2, FWidth);
-					fsq2 = lib.leftPublicShift(fsq2, FOffset);
-					T[] fsum = lib.padSignal(sum, FWidth);
-					fsum= lib.leftPublicShift(fsum, FOffset);
-
-					T[] div = flib.div(fsq, fsum);
-					resCase = flib.add(resCase, div);
-
-					T[] div2 = flib.div(fsq2, fsum);
-					resControl = flib.add(resControl, div2);
-				}
-				resCase = flib.div(resCase, flib.publicValue(1.0/200));
-				resControl = flib.div(resControl, flib.publicValue(1.0/200));
-				res[i] = flib.add(resCase, resControl);
-			}
-		}
-
-		@Override
-		public void prepareOutput(CompEnv<T> gen) {
-			for(int i = 0; i < numOfTests; ++i)
-				flib.outputToAlice(res[0]);
+				flib.outputToAlice(res[i]);
 		}
 	}
 }
