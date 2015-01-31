a427ea15dca4fb90b7ef657a6a2afcf4dd943aee
Xiao Wang <wangxiao1254@gmail.com>
2015-01-31 05:27:11 -0500

    ,

--- src/task1b/automated/task1b.cpp
+++ src/task1b/automated/task1b.cpp
@@ -1,14 +1,12 @@
 struct Task1b{};
 
-float32[public 1] Task1b.func(
-//      float32[public 200][public 3]alice_case,
-//      float32[public 200][public 3]alice_control,
-      float32[public 3] alice_case,
-      float32[public 3] alice_control,
-      float32[public 200][public 3]bob_case,
-      float32[public 3] bob_control,
+float32[public 200] Task1b.func(
+      float32[public 200][public 3] alice_case,
+      float32[public 200][public 3] alice_control,
+      float32[public 200][public 3] bob_case,
+      float32[public 200][public 3] bob_control,
       public int32 testcases ) {
-   float32[public 1] ret;
+   float32[public 200] ret;
    float32 resCase;
    float32 resControl;
    float32 tmp;
@@ -17,8 +15,8 @@ float32[public 1] Task1b.func(
       float32[public 3] control_freq;
       float32[public 3] case_freq;
       for(public int32 j = 0; j < 3; j = j + 1) {
-         tmp  = alice_control[j] + bob_control[j];
-//         case_freq[j] = alicecase[i][j] + bobcase[i][j];
+         control_freq[j]  = alice_control[i][j] + bob_control[i][j];
+         case_freq[j] = alice_case[i][j] + bob_case[i][j];
       }
 
       resCase = 0.0;
@@ -28,7 +26,7 @@ float32[public 1] Task1b.func(
          resControl = resControl +  (control_freq[k]*control_freq[k])/tmp;
          resCase = resCase +  (case_freq[k]*case_freq[k])/tmp;
       }
-      ret[i] =( resCase + resControl ) / 200;
+      ret[i] = ( resCase + resControl ) / 200.0;
    }
-   //   return ret;
+   return ret;
 }
