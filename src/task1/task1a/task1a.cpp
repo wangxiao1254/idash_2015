9a9a5a11e36534182cd1d41df2fcf356d19ea817
Xiao Wang <wangxiao1254@gmail.com>
2015-03-01 16:08:58 -0500

    .

--- src/task1/task1a/task1a.cpp
+++ src/task1/task1a/task1a.cpp
@@ -1,10 +1,10 @@
-struct Task1aAutomated@m{};
+struct Task1aAutomated@m@n{};
 
-void Task1aAutomated@m.funct(int@m[public 1] alice_data, int@m[public 1] bob_data,
-      int@m[public 1] ret, public int@m total_instances, public int32 test_cases) {
+void Task1aAutomated@m@n.funct(int@m[public n] alice_data, int@m[public n] bob_data,
+      int@m[public n] ret, public int@m total_instances) {
    int@m total = total_instances;
    int@m half = total_instances / 2;
-   for(public int32 i = 0; i < test_cases; i = i + 1) {
+   for(public int32 i = 0; i < n; i = i + 1) {
       ret[i] = alice_data[i] + bob_data[i];
       if(ret[i] > half)
          ret[i] = total - ret[i];
